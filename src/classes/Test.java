010e7386c7f80ce0c6e23e55a45a7087dd1e2da4
maxfiedler94 <maximilian.fiedler94@hotmail.com>
2016-11-29 09:43:38 +0100

    testar igen

--- src/classes/Test.java
+++ src/classes/Test.java
@@ -3,8 +3,12 @@ package classes;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("aaa");
-
+		System.out.println("aaa hej hej hej hej hej");
+		
+		System.out.println("mmMmmmM");
+		
+		
+		System.out.println("hwaehae");
 	}
 
 }
